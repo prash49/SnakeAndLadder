b04bb2ad59219ac72bd1cf3a336067a543ebf530
Prashanth <imprash4u@gmail.com>
2021-08-27 09:33:50 +0530

    Added: Player Randomly  Rolling Dice

--- src/main/java/com/bridglabz/snakeladder/SnakeAndLadder.java
+++ src/main/java/com/bridglabz/snakeladder/SnakeAndLadder.java
@@ -3,6 +3,7 @@ package com.bridglabz.snakeladder;
 public class SnakeAndLadder
 {
     public static void main( String[] args ){
-        int startPositiion = 0;
+        int startPosition = 0;
+        int diceNumber = (int) (Math.floor(Math.random() * 10) % 6 + 1);
     }
 }
