087edb06b5ce80a3bb7af7b0612c051b2f9faf10
Prashanth <imprash4u@gmail.com>
2021-08-27 10:07:27 +0530

    Added: resetting player position to 0 if he gets negative value

--- src/main/java/com/bridglabz/snakeladder/SnakeAndLadder.java
+++ src/main/java/com/bridglabz/snakeladder/SnakeAndLadder.java
@@ -5,20 +5,27 @@ public class SnakeAndLadder {
         int position = 0;
         int IS_SNAKE = 1;
         int IS_LADDER = 2;
+        int WINNING_POSITION = 100;
 
-        int diceNumber = (int) (Math.floor(Math.random() * 10) % 6 + 1);
+        while (position < WINNING_POSITION) {
 
-        int option = (int) (Math.floor(Math.random() * 10) % 2 + 1);
+            int diceNumber = (int) (Math.floor(Math.random() * 10) % 6 + 1);
 
-        if (option == IS_SNAKE) {
-            System.out.println("Snake for the Player");
-            position -= diceNumber;
-        } else if (option == IS_LADDER) {
-            System.out.println("ladder for the player");
-            position += diceNumber;
-        } else {
-            System.out.println("No Play for the Player");
+            int option = (int) (Math.floor(Math.random() * 10) % 2 + 1);
+
+            if (option == IS_SNAKE) {
+                System.out.println("Snake for the Player");
+                position -= diceNumber;
+                if (position < 0) {
+                    position = 0;
+                }
+            } else if (option == IS_LADDER) {
+                System.out.println("ladder for the player");
+                position += diceNumber;
+            } else {
+                System.out.println("No Play for the Player");
+            }
+            System.out.println("Position " + position);
         }
-        System.out.println("Position "+position);
     }
 }
