e7bf8eb38543036b5fee01dbe23d8c2fa889f590
Prashanth <imprash4u@gmail.com>
2021-08-27 09:56:25 +0530

    Added: Player options

--- src/main/java/com/bridglabz/snakeladder/SnakeAndLadder.java
+++ src/main/java/com/bridglabz/snakeladder/SnakeAndLadder.java
@@ -1,9 +1,24 @@
 package com.bridglabz.snakeladder;
 
-public class SnakeAndLadder
-{
-    public static void main( String[] args ){
-        int startPosition = 0;
+public class SnakeAndLadder {
+    public static void main(String[] args) {
+        int position = 0;
+        int IS_SNAKE = 1;
+        int IS_LADDER = 2;
+
         int diceNumber = (int) (Math.floor(Math.random() * 10) % 6 + 1);
+
+        int option = (int) (Math.floor(Math.random() * 10) % 2 + 1);
+
+        if (option == IS_SNAKE) {
+            System.out.println("Snake for the Player");
+            position -= diceNumber;
+        } else if (option == IS_LADDER) {
+            System.out.println("ladder for the player");
+            position += diceNumber;
+        } else {
+            System.out.println("No Play for the Player");
+        }
+        System.out.println("Position "+position);
     }
 }
